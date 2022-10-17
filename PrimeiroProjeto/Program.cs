49629e58282e6e1821df8f4d6e9e4420e8bc7c02
Walber S Mota <walbersmota@gmail.com>
2022-10-17 01:53:12 -0300

    Atualizando as pastas

---
+++ PrimeiroProjeto/Program.cs
@@ -0,0 +1,17 @@
+﻿using System; //informa que o namespace do sistem padrão
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeiroProjeto //namespace que eu criei no meu projeto
+{
+    class Program //a classe que eu criei - preferir o nome arquivo .cs
+   {
+        static void Main(string[] args) //mostra o posto de entrada do programa em si.
+        {
+            Console.WriteLine("Olá, Mundo!"); //chama a classe console do namespace do sistema
+            Console.ReadKey(); //espera o usuario apertar qualquer tecla para terminar o app
+        }
+    }
+}
---
+++ SaidaDeVariaveis/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace SaidaDeVariaveis {
+    class Program {
+        static void Main(string[] args) {
+            char genero = 'F';
+            int idade = 32;
+            double saldo = 12500.35784;
+            string nome = "Maria";
+
+            Console.WriteLine("Nome : " + nome); //
+            Console.WriteLine("Sexo : " + genero);
+            Console.WriteLine("Idade : " + idade + "anos");
+            //saida com virgula de separador decimal, configuração do computador.
+            Console.WriteLine("Salário : R$ " + saldo);
+            Console.WriteLine("Salário : R$ " + saldo.ToString("f2"));
+            //ponto como separador decimal
+            Console.WriteLine("Salário : R$ " + saldo.ToString("f2", CultureInfo.InvariantCulture));
+            Console.WriteLine("");
+
+            Console.WriteLine("1 - usando Placeholders: {0} tem {1} anos e recebe {2:f2} reais,nome,idade,saldo");
+            Console.WriteLine("{0} tem {1} anos e recebe {2:f2} reais", nome, idade, saldo);
+            Console.WriteLine("");
+
+            Console.WriteLine("2 - usando Interpolação: {nome} tem {idade} anos e recebe {saldo:f2} reais");
+            Console.WriteLine($"{nome} tem {idade} anos e recebe {saldo:f2} reais");
+
+            Console.WriteLine("");
+            Console.WriteLine("tecla algo para fechar...git ");
+            Console.ReadKey();
+        }
+    }
+}
