23d2019ab179ab71376e5ccf4659cfacb5cd14a4
WalberMota <mota.csharp@gmail.com>
2022-11-17 19:24:30 -0300

    split numbers

---
+++ SeparaNumero/SeparaNumero/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ExtrairNumeros {
+    internal class Program {
+        static void Main(String[] args) {
+            int modulo, tamanho;
+            String n2 = "";
+            string numinicial = "";
+            Console.Write("Digite um numero qualquer com quantidade de digitos:");
+            numinicial = Console.ReadLine();
+            tamanho = numinicial.Length;
+            for (int i = 0; i < tamanho; i++) {
+
+                modulo = int.Parse(numinicial) % 10;
+                n2 = Convert.ToString(modulo) + "   " + n2;
+                numinicial = Convert.ToString(int.Parse(numinicial) / 10);
+                //Console.WriteLine(numinicial);
+                //Console.WriteLine(n2);
+            }
+            Console.WriteLine(n2);
+            Console.ReadKey();
+
+        }
+    }
+}
+
