82ef8e9554a5acaa8b4ee3eb3f3ce8c901a65de6
WalberMota <mota.csharp@gmail.com>
2022-12-19 20:07:11 -0300

    vscode

---
+++ LerGravaArqtxt01/LerGravaArqtxt01/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.IO;
+using System.Net.WebSockets;
+using System.Windows;
+
+namespace LerGravaArqtxt01 {
+    internal class Program {
+        static void Main(string[] args) {
+            string dirDocumentos = (string)Microsoft.Win32.Registry.CurrentUser
+                .OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders")
+                .GetValue("Personal"); //localiza a pasta documentos do usuários
+
+            string acao;
+            string caminho = dirDocumentos + "\\usuario.txt";//cria o arquivo usuario.txt
+            string nome, email, telefone, rg;
+
+            Console.WriteLine("##########################");
+            Console.WriteLine("##########################");
+            Console.WriteLine("###### G - Gravar ########");
+            Console.WriteLine("###### L - Ler    ########");
+            Console.WriteLine("###### S - SAIR   ########");
+            Console.WriteLine("##########################");
+            Console.WriteLine(" X - Apaga todos os dados ");
+            Console.WriteLine("##########################");
+            Console.WriteLine();
+            Console.Write("Digite uma opção: ");
+            acao = Console.ReadLine().ToUpper();
+            Console.WriteLine();
+            while (acao != "S") {
+
+                if (acao == "G") { //   adicionar item ao arquivo texto
+
+                    Console.Write("Informe o nome: ");
+                    nome = Console.ReadLine().ToUpper();
+
+                    Console.Write("Informe o email: ");
+                    email = Console.ReadLine().ToUpper();
+
+                    Console.Write("Informe o telefone: ");
+                    telefone = Console.ReadLine().ToUpper();
+
+                    Console.Write("Informe o RG: ");
+                    rg = Console.ReadLine().ToUpper();
+
+                    StreamWriter sw = new StreamWriter(caminho, true);
+                    sw.WriteLine("Nome:     " + nome);
+                    sw.WriteLine("email:    " + email);
+                    sw.WriteLine("Telefone: " + telefone);
+                    sw.WriteLine("Rg:       " + rg);
+                    sw.WriteLine("---------------------------------------");
+                    sw.Close();
+                }
+                else if (acao == "L") {
+
+                    StreamReader sr = new StreamReader(caminho);
+
+                    while (sr.EndOfStream != true) {
+                        Console.WriteLine(sr.ReadLine());
+                    }
+                    sr.Close();
+                }
+                else if (acao == "X") {
+                    StreamWriter zerar = new StreamWriter(caminho, false);
+                    zerar.WriteLine("");
+                    zerar.Close();
+                }
+                Console.WriteLine();
+                Console.WriteLine("Pressione uma tecla para continuar...");
+                Console.ReadKey();
+                Console.Clear();
+
+                Console.WriteLine("##########################");
+                Console.WriteLine("##########################");
+                Console.WriteLine("###### G - Gravar ########");
+                Console.WriteLine("###### L - Ler    ########");
+                Console.WriteLine("###### S - SAIR   ########");
+                Console.WriteLine("##########################");
+                Console.WriteLine(" X - Apaga todos os dados ");
+                Console.WriteLine("##########################");
+                Console.WriteLine();
+                Console.Write("Digite uma opção: ");
+                acao = Console.ReadLine().ToUpper();
+                Console.WriteLine();
+
+            }
+
+            Console.BackgroundColor = ConsoleColor.DarkGray;
+            Console.Clear();
+            MessageBox.Show("SYSTEM CLOSED", "by walber 19/11/2022");
+        }
+    }
+
+}
--- ReplaceWH/ReplaceWH/Program.cs
+++ ReplaceWH/ReplaceWH/Program.cs
@@ -7,17 +7,34 @@ using System.Threading.Tasks;
 namespace ReplaceWH {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Digite un texto");
+            Console.Title= "Hacker text by Walber Mota - November 11, 2022";
+            Console.WriteLine("              CONVERSOR HACKER TEXT");
+            Console.WriteLine(".............................................");
+            Console.WriteLine("           Quando terminar aperte enter");
+            Console.WriteLine(".............................................");
+            Console.WriteLine();
+            Console.WriteLine();
             string source = Console.ReadLine();
+
 
             // Replace all occurrences of one char with another.
             var repo1 = source.Replace('t', '7');
             var repo2 = repo1.Replace('T', '7');
             var repo3 = repo2.Replace('a', '4');
-            var repo4 = repo3.Replace('i', '1');
-            var repo5 = repo4.Replace('e', '3');
-            //Console.WriteLine(source);
-            Console.WriteLine(repo5);
+            var repo4 = repo3.Replace('A', '4');
+            var repo5 = repo4.Replace('i', '1');
+            var repo6 = repo5.Replace('I', '1');
+            var repo7 = repo6.Replace('e', '3');
+            var repo8 = repo7.Replace('E', '3');
+            var repo9 = repo8.Replace('o', '0');
+            var repo10 = repo9.Replace('O', '0');
+
+            Console.WriteLine("\n\n.............................................");
+            
+            Console.WriteLine("\nTexto convertido :");
+            Console.WriteLine("\n"+repo10);
+            Console.WriteLine();
+            Console.ReadKey();
         }
     }
 }
